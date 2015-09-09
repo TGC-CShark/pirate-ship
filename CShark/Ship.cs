39a25311ba0dcb31ea9af2d09403bd801dd0bd76
Sandro Damilano <damilano.sandro@gmail.com>
2015-09-09 16:19:48 -0300

    agregado barquito cubo :D

---
+++ CShark/Ship.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.DirectX;
+using Microsoft.DirectX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using TgcViewer;
+using TgcViewer.Utils.Input;
+using TgcViewer.Utils.TgcGeometry;
+
+namespace AlumnoEjemplos.CShark
+{
+    class Ship
+    {
+            TgcBox box;
+            Vector3 position;
+            Vector3 size;
+            float movementSpeed=50;
+            Vector3 movement;
+
+            public Ship(Vector3 pos)
+            {
+                position = pos;
+                Vector3 size = new Vector3(10, 10, 10);
+                Color color = Color.Red;
+                box = TgcBox.fromSize(position, size, color);
+                
+            }
+
+            public void renderizar(){
+                box.render();
+            }
+
+           public void actualizar(float elapsedTime){
+
+               movement = InputHandler.getMovement();
+               movement *= movementSpeed * elapsedTime;
+               box.move(movement);
+           }
+
+
+
+           internal void dispose()
+           {
+               box.dispose();
+           }
+    }
+}
