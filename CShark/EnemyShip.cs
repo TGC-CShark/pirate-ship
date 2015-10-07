22cdf15941df10982f584082be0b5b087d0aef87
Matias Martino <matiasfmartino@gmail.com>
2015-10-07 11:49:39 -0300

    Ahora el enemigo sigue al barco del jugador (hay que suavizar los movimientos, y mejorar las des/asceleraciones)

--- CShark/EnemyShip.cs
+++ CShark/EnemyShip.cs
@@ -14,12 +14,11 @@ namespace AlumnoEjemplos.CShark
 {
     class EnemyShip : Ship
     {
-
         Ship player;
 
-        const float ROTATION_SPEED = 2f;
+        const float ROTATION_SPEED = 1f;
         const float VEL_MAXIMA = 500f;
-        const float ESCALON_VEL = 0.4f;
+        const float ESCALON_VEL = 0.6f;
 
         public EnemyShip(Ship player, Vector3 pos, TgcMesh mesh, Canion canion) : base(pos, mesh, canion) 
         { 
@@ -29,19 +28,6 @@ namespace AlumnoEjemplos.CShark
 
         public override void calcularTraslacionYRotacion(float elapsedTime)
         {
-            if (GuiController.Instance.D3dInput.keyDown(Key.H))
-            {
-                anguloRotacion -= elapsedTime * ROTATION_SPEED;
-                rotacion = Matrix.RotationY(anguloRotacion);
-            }
-
-            else if (GuiController.Instance.D3dInput.keyDown(Key.K))
-            {
-                anguloRotacion += elapsedTime * ROTATION_SPEED;
-                rotacion = Matrix.RotationY(anguloRotacion);
-
-            }
-
             Vector3 distance = player.popa() - this.getPosition();
             Vector3 iaDirectionVersor = this.vectorDireccion();
             iaDirectionVersor.Normalize();
@@ -65,37 +51,19 @@ namespace AlumnoEjemplos.CShark
                 rotacion = Matrix.RotationY(anguloRotacion);
             }
 
-            //anguloRotacion += rotationAngle;
-
-           // rotacion = Matrix.RotationY(rotationAngle);
-
-
-            //float delta = FastMath.Atan2(diff.X, diff.Z);
-
-            //float correcionGiro = elapsedTime * ROTATION_SPEED ;
-            //anguloRotacion = delta < 0 ? anguloRotacion - correcionGiro : anguloRotacion + correcionGiro;
-            //anguloRotacion += elapsedTime * ROTATION_SPEED * delta;   
-     
-            //rotacion = Matrix.RotationY(anguloRotacion);
-
-/*
-            rotacion = Matrix.RotationY(player.anguloRotacion);
-
-            if (FastMath.Abs(distance(player)) > 300)
+            if (FastMath.Abs(distance.Length()) > 200)
             {
-                movementSpeed = VEL_MAXIMA / 4;
+                movementSpeed = Math.Min(movementSpeed + ESCALON_VEL, VEL_MAXIMA);
             }
-            if (FastMath.Abs(distance(player)) < 300)
+            if (FastMath.Abs(distance.Length()) < 400)
             {
-                movementSpeed = 0;
+                movementSpeed = Math.Max(movementSpeed - movementSpeed / distance.Length(), 0);
             }
-
 
             movZ -= movementSpeed * FastMath.Cos(anguloRotacion) * elapsedTime;
             movX -= movementSpeed * FastMath.Sin(anguloRotacion) * elapsedTime;
-            traslacion = Matrix.Translation(diff.X * 5, 0, diff.Z * 5);
-*/            
-            //traslacion = Matrix.Translation(diff.X * 5, 0, diff.Z * 5);
+            traslacion = Matrix.Translation(movX, 0, movZ);
+
             //Cargar valor en UserVar
             GuiController.Instance.UserVars.setValue("dir_p", player.vectorDireccion());
             GuiController.Instance.UserVars.setValue("dir_ia", this.vectorDireccion());
@@ -103,7 +71,7 @@ namespace AlumnoEjemplos.CShark
             GuiController.Instance.UserVars.setValue("pos_ia", this.getPosition());
             GuiController.Instance.UserVars.setValue("popa_p", player.popa());
             GuiController.Instance.UserVars.setValue("popa_ia", this.popa());
-            GuiController.Instance.UserVars.setValue("distancia", distance);
+            GuiController.Instance.UserVars.setValue("distancia", distance.Length());
             GuiController.Instance.UserVars.setValue("dist_normalizada", lookAtPopaVersor);           
             GuiController.Instance.UserVars.setValue("angulo_rotacion", rotationAngle * 360 / FastMath.PI);
             GuiController.Instance.UserVars.setValue("cross_product", Vector3.Cross(lookAtPopaVersor, iaDirectionVersor));
